089bff4c1f83d30c424e5a74a12885d5ef13a4b3
Sorin Sbarnea <ssbarnea@redhat.com>
2021-12-16 21:16:41 +0000

    Bump linters

--- molecule_openstack/test/unit/test_driver.py
+++ molecule_openstack/test/unit/test_driver.py
@@ -2,5 +2,5 @@ from molecule import api
 
 
 def test_driver_is_detected():
-    driver_name = __name__.split(".")[0].split("_")[-1]
+    driver_name = __name__.split(".", maxsplit=1).split("_")[-1]
     assert driver_name in [str(d) for d in api.drivers()]
